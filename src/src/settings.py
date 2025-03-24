2c2007ca81ae9b7a47e57d017840522a711e3f0c
trieuconcrete <trieunb@concrete-corp.com>
2025-03-24 10:22:03 +0700

    update

--- src/src/settings.py
+++ src/src/settings.py
@@ -148,12 +148,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
+    "/opt/dj_project/src/static/",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where static files will be collected
 
