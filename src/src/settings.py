4b6b19d4ea7185d224a497506b6ae293e55c58ab
trieuconcrete <trieunb@concrete-corp.com>
2025-03-24 10:00:31 +0700

    update

--- src/src/settings.py
+++ src/src/settings.py
@@ -182,13 +182,13 @@ CKEDITOR_CONFIGS = {
 }
 
 # Email configuration settings
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # show console
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = env('EMAIL_HOST')
-# EMAIL_PORT = env('EMAIL_PORT')
-# EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # show console
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
 #
 #
 # # Be sure to read the guide in the resource folder of this lecture (SETUP THE EMAIL BACKEND)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
